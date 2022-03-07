84d79b74287fd9a3508ad24fb34b42e25cd47bff
신범호 <shinn0608@naver.com>
2022-03-07 11:44:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import ApiReq from './pages/ApiReq';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/">Home</Link>  <br/>
+      <Link to="/about">소개</Link> <br/>
+      <Link to="/api">API 호출</Link>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/api" element={<ApiReq/>} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
